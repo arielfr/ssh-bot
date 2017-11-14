cbe6e9046c8b6c211b2f8e427bb6946c6f6a3ed2
ARIEL REY <ariel.rey@mercadolibre.com>
2017-11-14 14:46:19 -0300

    Adding express basic application. Adding logger dependencies

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 /**
  * Created by arey on 11/14/17.
  */
+const express = require('express');
+const logger = require('winston-this')('index');
 const node_ssh = require('node-ssh');
 
-ssh = new node_ssh();
+// Express Application initialization
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.listen(3000, function () {
+  logger.info('Example application listening on port 3000');
+});
+
+/*
+const ssh = new node_ssh();
 
 ssh.connect({
   host: '13.84.178.228',
@@ -17,3 +31,4 @@ ssh.connect({
     ssh.dispose();
   });
 });
+*/
